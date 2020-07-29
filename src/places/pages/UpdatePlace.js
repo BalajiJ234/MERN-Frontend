4d973f4eee9197602a5a2c8c92b8d81acc0ae6b8
Balaji Janarthanan <31264281+BalajiJ234@users.noreply.github.com>
2020-07-29 19:05:56 +0530

    Adjusting the Input Component

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,7 +1,91 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const UpdatePlace = (props) =>{
-  return <h2>Update Place</h2>
-}
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import { VALIDATOR_REQUIRE, VALIDATOR_MAXLENGTH } from "../../shared/util/validators";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrappers in the world",
+    imageUrl:
+      "https://www.history.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_686/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.webp",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484278,
+      lng: -73.9881525,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrappers in the world",
+    imageUrl:
+      "https://www.history.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_686/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.webp",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484278,
+      lng: -73.9881525,
+    },
+    creator: "u2",
+  },
+];
+
+const UpdatePlace = (props) => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find the place</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MAXLENGTH(5)]}
+        errorText="Please enter a valid description(atleast 5 Characters)."
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Input
+        id="address"
+        element="input"
+        type="text"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
+        onInput={() => {}}
+        value={identifiedPlace.address}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
 
 export default UpdatePlace;

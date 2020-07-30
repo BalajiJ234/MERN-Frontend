88139db15e8bb0ebbd73512b55c2838386821c5b
Balaji Janarthanan <31264281+BalajiJ234@users.noreply.github.com>
2020-07-30 10:29:28 +0530

    Adjusting the Input Component,

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,54 +1,58 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import { VALIDATOR_REQUIRE, VALIDATOR_MAXLENGTH } from "../../shared/util/validators";
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrappers in the world",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://www.history.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_686/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.webp",
-    address: "20 W 34th St, New York, NY 10001, United States",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 40.7484278,
-      lng: -73.9881525,
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    creator: "u1",
+    creator: 'u1'
   },
   {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrappers in the world",
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://www.history.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_686/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.webp",
-    address: "20 W 34th St, New York, NY 10001, United States",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 40.7484278,
-      lng: -73.9881525,
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    creator: "u2",
-  },
+    creator: 'u2'
+  }
 ];
 
-const UpdatePlace = (props) => {
+const UpdatePlace = () => {
   const placeId = useParams().placeId;
 
-  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find the place</h2>
+        <h2>Could not find place!</h2>
       </div>
     );
   }
 
   return (
-    <form>
+    <form className="place-form">
       <Input
         id="title"
         element="input"
@@ -64,21 +68,10 @@ const UpdatePlace = (props) => {
         id="description"
         element="textarea"
         label="Description"
-        validators={[VALIDATOR_MAXLENGTH(5)]}
-        errorText="Please enter a valid description(atleast 5 Characters)."
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
         onInput={() => {}}
         value={identifiedPlace.description}
-        valid={true}
-      />
-      <Input
-        id="address"
-        element="input"
-        type="text"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={() => {}}
-        value={identifiedPlace.address}
         valid={true}
       />
       <Button type="submit" disabled={true}>
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,56 +1,57 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useEffect } from 'react';
 
-import { validate } from "../../util/validators";
-import "./Input.css";
+import { validate } from '../../util/validators';
+import './Input.css';
 
 const inputReducer = (state, action) => {
   switch (action.type) {
-    case "CHANGE":
+    case 'CHANGE':
       return {
         ...state,
         value: action.val,
-        isValid: validate(action.val, action.validators),
+        isValid: validate(action.val, action.validators)
       };
-    case "TOUCH":
+    case 'TOUCH': {
       return {
         ...state,
-        isTouched: true,
-      };
+        isTouched: true
+      }
+    }
     default:
       return state;
   }
 };
 
-const Input = (props) => {
+const Input = props => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: props.value || '',
-    isValid: false,
-    isTouched: props.valid || false,
+    isTouched: false,
+    isValid: props.valid || false
   });
 
   const { id, onInput } = props;
   const { value, isValid } = inputState;
 
   useEffect(() => {
-    onInput(id, value, isValid);
+    onInput(id, value, isValid)
   }, [id, value, isValid, onInput]);
 
-  const touchHandler = (event) => {
+  const changeHandler = event => {
     dispatch({
-      type: "TOUCH",
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators
     });
   };
 
-  const changeHandler = (event) => {
+  const touchHandler = () => {
     dispatch({
-      type: "CHANGE",
-      val: event.target.value,
-      validators: props.validators,
+      type: 'TOUCH'
     });
   };
 
   const element =
-    props.element === "input" ? (
+    props.element === 'input' ? (
       <input
         id={props.id}
         type={props.type}
@@ -71,9 +72,8 @@ const Input = (props) => {
 
   return (
     <div
-      className={`form-control ${
-        !inputState.isValid && inputState.isTouched && "form-control--invalid"
-      }`}
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        'form-control--invalid'}`}
     >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
